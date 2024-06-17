fcc2a7d800147c8e273396017965ec986d2efb7d
josselinbarry <josselin.barry@ofb.gouv.fr>
2024-06-17 12:35:02 +0200

    maj synth territoires

--- w_11_pe_analyse.R
+++ w_11_pe_analyse.R
@@ -18,10 +18,23 @@ library(sf)
 #library(stringi)
 library(units)
 
+
 ## Chargement des données ----
 
 pe <-
-  sf::read_sf(dsn = "data/outputs/pe_qualifies_20231202.gpkg")
+  sf::read_sf(dsn = "data/outputs/pe_qualifies_20240220.gpkg")
+
+dprt <-
+  sf::read_sf(dsn = "data/departements.gpkg") %>%
+  st_transform(crs = 2154) %>%
+  mutate(surface_km2 = st_area(geom)/1000000) %>%
+  units::drop_units() %>%
+  mutate(insee_reg = ifelse((insee_dep == 22 |
+                               insee_dep == 29 |
+                               insee_dep == 35 |
+                               insee_dep == 56), '53', '52'))
+         
+         
 
 bv_me <-
   sf::read_sf(dsn = "data/outputs/bv_me_decoup_20240110.gpkg")
@@ -67,61 +80,109 @@ histo_pe_persistance_surface <-
 
 histo_pe_persistance_surface
 
-# Tableur répartition PE selon type ----
+# Tableur répartition PE selon type et territoire ----
 
 synth_pe <- 
-  pe_decoup_com %>%
-  filter(insee_dep %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85)) %>%
+  pe %>%
+  filter(cd_dprt %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85)) %>%
   sf::st_drop_geometry() %>% 
-  select(insee_dep, insee_reg, Persistanc, StreamOrde, zone_marais, mare) %>%
+  select(cd_dprt, Persistanc, StreamOrde, zone_marais, mare) %>%
   as.data.frame() %>%
-  group_by(insee_reg,insee_dep) %>%
+  group_by(cd_dprt) %>%
   sf::st_drop_geometry() %>% 
   summarise(nb_pe_tot = n()) 
 
 synth_pe_hors_marais <- 
-  pe_decoup_com %>%
-  filter(insee_dep %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
+  pe %>%
+  filter(cd_dprt %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
            zone_marais == 0) %>%
   sf::st_drop_geometry() %>% 
-  select(insee_dep, insee_reg, Persistanc, StreamOrde, zone_marais, mare) %>%
+  select(cd_dprt, Persistanc, StreamOrde, zone_marais, mare) %>%
   as.data.frame() %>%
-  group_by(insee_reg,insee_dep) %>%
+  group_by(cd_dprt) %>%
   sf::st_drop_geometry() %>% 
   summarise(nb_pe_hors_marais = n()) 
 
 synth_pe_hors_mare <- 
-  pe_decoup_com %>%
-  filter(insee_dep %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
+  pe %>%
+  filter(cd_dprt %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
            mare == 0 ) %>%
   sf::st_drop_geometry() %>% 
-  select(insee_dep, insee_reg, Persistanc, StreamOrde, zone_marais, mare) %>%
+  select(cd_dprt, Persistanc, StreamOrde, zone_marais, mare) %>%
   as.data.frame() %>%
-  group_by(insee_reg,insee_dep) %>%
+  group_by(cd_dprt) %>%
   sf::st_drop_geometry() %>% 
   summarise(nb_pe_hors_mare = n()) 
 
 synth_pe_mare_marais <- 
-  pe_decoup_com %>%
-  filter(insee_dep %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
+  pe %>%
+  filter(cd_dprt %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
            mare == 0 &
            zone_marais == 0) %>%
   sf::st_drop_geometry() %>% 
-  select(insee_dep, insee_reg, Persistanc, StreamOrde, zone_marais, mare) %>%
+  select(cd_dprt, Persistanc, StreamOrde, zone_marais, mare) %>%
   as.data.frame() %>%
-  group_by(insee_reg,insee_dep) %>%
+  group_by(cd_dprt) %>%
   sf::st_drop_geometry() %>% 
   summarise(nb_pe_hors_mare_marais = n()) 
 
-regions %>%
+synth_pe_mare_marais_perm <- 
+  pe %>%
+  filter(cd_dprt %in% c(22, 29, 35, 44, 49, 53, 56, 72, 85) &
+           mare == 0 &
+           zone_marais == 0 & 
+           Persistanc == 'permanent') %>%
+  sf::st_drop_geometry() %>% 
+  select(cd_dprt, Persistanc, StreamOrde, zone_marais, mare) %>%
+  as.data.frame() %>%
+  group_by(cd_dprt) %>%
+  sf::st_drop_geometry() %>% 
+  summarise(nb_pe_hors_mare_marais_perm = n()) 
+
+dprt <- dprt %>%
   st_drop_geometry() %>%
-  select(insee_reg,nom_region) %>%
-  left_join(synth_pe) %>%
-  left_join(synth_pe_hors_marais) %>%
-  left_join(synth_pe_hors_mare) %>%
-  left_join(synth_pe_mare_marais) %>%
+  left_join(synth_pe, by = c("insee_dep" = "cd_dprt")) %>%
+  left_join(synth_pe_hors_marais, by = c("insee_dep" = "cd_dprt")) %>%
+  left_join(synth_pe_hors_mare, by = c("insee_dep" = "cd_dprt")) %>%
+  left_join(synth_pe_mare_marais, by = c("insee_dep" = "cd_dprt")) %>%
+  left_join(synth_pe_mare_marais_perm, by = c("insee_dep" = "cd_dprt"))
+  
+synth_region <- dprt %>%
+  group_by(insee_reg) %>%
+  summarise(surface_km2 = sum(surface_km2),
+            nb_pe_tot = sum(nb_pe_tot),
+            nb_pe_hors_marais = sum(nb_pe_hors_marais),
+            nb_pe_hors_mare = sum(nb_pe_hors_mare),
+            nb_pe_hors_mare_marais = sum(nb_pe_hors_mare_marais),
+            nb_pe_hors_mare_marais_perm = sum(nb_pe_hors_mare_marais_perm))
+
+synthese_territoire <-
+  dplyr::bind_rows(dprt %>%
+                     mutate(nom = nom_departement), 
+                   synth_region %>%
+                     mutate(nom = ifelse(insee_reg == 52, 'Pays de la Loire', 'Bretagne')))
+
+synthese_territoire <- synthese_territoire %>%
+  mutate(dens_pe_tot = nb_pe_tot/surface_km2,
+         dens_pe_hors_marais = nb_pe_hors_marais/surface_km2,
+         dens_pe_hors_mare = nb_pe_hors_mare/surface_km2,
+         dens_pe_hors_mare_marais = nb_pe_hors_mare_marais/surface_km2,
+         dens_pe_hors_mare_marais_perm = nb_pe_hors_mare_marais_perm/surface_km2,) %>%
+  select(nom, 
+        surface_km2,
+        nb_pe_tot, 
+        dens_pe_tot,
+        nb_pe_hors_marais,
+        dens_pe_hors_marais,
+        nb_pe_hors_mare,
+        dens_pe_hors_mare,
+        nb_pe_hors_mare_marais,
+        dens_pe_hors_mare_marais,
+        nb_pe_hors_mare_marais_perm,
+        dens_pe_hors_mare_marais_perm)
+  
+openxlsx::write.xlsx(synthese_territoire, file = "data/outputs/synthese_territoires.xls" )
+
+synthese_territoire %>%
   kbl() %>%
   kable_styling(bootstrap_options = c("striped", "hover", "condensed"), font_size = 12) 
-
-
-
